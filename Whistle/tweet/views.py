5f836e5cfbaaf9a15373475469cd21184297879f
siddharthk2608 <siddharth.k@consultadd.com>
2025-08-28 03:33:55 +0530

    registered models with admin and created TweetForms

--- Whistle/tweet/views.py
+++ Whistle/tweet/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render('request','tweet_list.html', {'tweets':tweets})
+
+
+def tweet_create(request):
+    if request.method == "POST":
+        pass
+    else : 
+        form = TweetForm()
+    return render('request', "tweet_form.html",{'form':form})
+        
